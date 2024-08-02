7bf3f0a32caf3d57a97de50e2d8b26d65d1ef70e
jakaria242 <jakaria.dev242@gmail.com>
2024-08-02 12:12:35 +0600

    authReducerSlice & AdminLogin work

--- src/store/Reducers/authReducerSlice.js
+++ src/store/Reducers/authReducerSlice.js
@@ -3,13 +3,15 @@ import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
-    async  (info)=>{
-        console.log(info);
+    async  (info, { rejectWithValue, fulfillWithValue })=>{
+        // console.log(info);
         try {
            const { data }  = await api.post("/admin-login", info, {withCredentials : true}) 
-           console.log(data);
+        //    console.log(data);
+          return fulfillWithValue(data)
         } catch (error) {
-            console.log("admin login thunk error ", error);
+            // console.log( error.response.data);
+            return  rejectWithValue(error.response.data)
         }
     }
 )
@@ -22,13 +24,30 @@ export const authReducerSlice = createSlice({
         userInfo: ""
     },
     reducers: {
-
+        messageClear : (state, action)=>{
+            state.errorMessage = ""
+            state.successMessage = ""
+        }
     },
-    extraReducers : (builder) =>{
+    extraReducers : (builder) => {
+        
+            builder.addCase(admin_login.pending, (state,_) => {
+                state.loder = true;
+            });
 
+         builder.addCase(admin_login.rejected, (state, {payload}) => {
+            state.loder = false;
+            state.errorMessage = payload.error
+         });
+         builder.addCase(admin_login.fulfilled, (state, {payload}) => {
+            state.loder = false;
+            state.successMessage = payload.message
+         });
     }
 
 }) 
+
+export const { messageClear } = authReducerSlice.actions
 
 export default authReducerSlice.reducer
 
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
-import { admin_login } from '../../store/Reducers/authReducerSlice'
-import {  useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { admin_login, messageClear } from '../../store/Reducers/authReducerSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { PropagateLoader } from 'react-spinners'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 
 const AdminLogin = () => {
-
+  const navigate = useNavigate()
   const dispatch = useDispatch()
+  const { loader, errorMessage, successMessage } = useSelector(state => state.auth)
 
     const [adminLogin, setAdminLogin] = useState({
         email: "",
@@ -24,6 +28,27 @@ const AdminLogin = () => {
         e.preventDefault()
         dispatch(admin_login(adminLogin));
       }
+
+      const overrideStyle = {
+        display: "flex",
+        margin: "0 auto",
+        hight: "24px",
+        justifyContent: "center",
+        alignItems: "center"
+      }
+
+      useEffect(()=>{
+        if (errorMessage) {
+           toast.error(errorMessage)
+           dispatch(messageClear())
+        }
+        if (successMessage) {
+           toast.success(successMessage)
+           dispatch(messageClear())
+           navigate('/')
+        }
+
+      },[errorMessage, successMessage])
 
   return (
     <div className='min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center'>
@@ -49,7 +74,11 @@ const AdminLogin = () => {
                 <label htmlFor="password">Password</label>
                 <input onChange={inputHandle} value={adminLogin.password}  className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="password" name='password' placeholder='password' id='password' required/>
             </div>
-            <button className='w-full capitalize bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Login</button>
+            <button disabled={loader? true: false} className='w-full capitalize bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+            {
+              loader? <PropagateLoader color='#fff' cssOverride={overrideStyle} /> : "Login"
+            }
+            </button>
           </form>
        </div>
     </div>
