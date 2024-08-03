e49bd3756a8ff021a8fe96be44addbdf1a6a209c
jakaria242 <jakaria.dev242@gmail.com>
2024-08-03 20:13:59 +0600

    dashboard  ar header & chart some design done

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = ({showSidebar, setShowSidebar}) => {
 
   return (
     <div>
-       <div onClick={()=>setShowSidebar(false)} className={`fixed duration-200 ${!setShowSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#22292f80] top-0 left-10 z-10`}></div>
+       <div onClick={()=>setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#22292f80] top-0 left-10 z-10`}></div>
        <div className={`w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0': '-left-[260px] lg:left-0'}`}>
            {/*
           
